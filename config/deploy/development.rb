fe49d279070e6b5a669a7ad4e8571c07e5daa8bb
darkwarlords <darkwarlords@sina.com>
2018-01-08 18:04:55 +0800

    no message

--- config/deploy/development.rb
+++ config/deploy/development.rb
@@ -5,6 +5,11 @@ set :rvm_type, :user
 set :rvm_ruby_version, '2.3.3'
 set :deploy_user, 'master'
 
+# bundle 时候排除 test
+set :bundle_without, ['test']
+# 开发、静默
+set :bundle_flags, '--deployment --quiet'
+
 server '39.106.190.128', user: 'master', roles: %w{web app db}, port: 22, primary: true
 
 set :deploy_to, "/srv/www/api"
