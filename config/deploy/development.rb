a24dce0ec11f1b677c8ccf3b4550c209bcf4dac0
darkwarlords <darkwarlords@sina.com>
2018-01-08 18:16:34 +0800

    no message

--- config/deploy/development.rb
+++ config/deploy/development.rb
@@ -5,7 +5,7 @@ set :rvm_type, :user
 set :rvm_ruby_version, '2.3.3'
 set :deploy_user, 'master'
 
-# bundle 时候排除 test
+# bundle 时候排除 group:test gems
 set :bundle_without, ['test']
 # 开发、静默
 set :bundle_flags, '--deployment --quiet'
