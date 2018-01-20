5e5697ca35f17eb43762dc320c7a59a60a93950e
darkwarlords <darkwarlords@sina.com>
2018-01-20 10:30:15 +0800

    no message

--- apis/shop/v1/orders_api.rb
+++ apis/shop/v1/orders_api.rb
@@ -48,14 +48,47 @@ module V1
           params do
             requires :user_uuid, type: String, desc: '用户 UUID'
             requires :token, type: String, desc: '用户访问令牌'
-            requires :category, type: String, desc: '按分类查询'
+            optional :category, type: String, desc: "按分类查询，默认为全部，{awaiting_payment: '待付款', scrape_together: '拼单中', waiting_for_delivery: '待发货', waiting_for_received: '待收货', waiting_evaluation: '待评价'}"
           end
           get do
-            [
-              {
-                
-              }
-            ]
+            {
+              orders: [
+                {
+                  uuid: '11fewfaef',
+                  shop: {
+                    name: '店铺名称',
+                    logo: 'https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1516322006&di=65ee9624697c83b5dcb3292b347d2462&imgtype=jpg&er=1&src=http%3A%2F%2Fimg.zcool.cn%2Fcommunity%2F01c2ac57beb18d0000012e7eaa6d19.jpg',
+                    scheme: 'www.baidu.com',
+                  },
+                  state: '拼单中',
+                  product: {
+                    image: 'http://img.mshishang.com/pics/2015/1118/20151118021532201.jpg',
+                    title: '恒都 澳洲牛腩块 500克/袋 草饲牛肉 包邮',
+                    style_name: '红色 41码',
+                    price: '¥ 30.00',
+                    quantity_str: 'x5'
+                  },
+                  pay_amount: '实付 ¥20.00',
+                  pay_center_scheme: nil,
+                  buy_again_scheme: nil,
+                  look_logistics_scheme: nil,
+                  to_evaluate_scheme: nil,
+                  removeable: true,
+                  can_be_hasten: true,
+                  confirmable: true,
+                  inviting_friends_info: nil
+                  # 立即支付
+#                   再次购买
+#                   查看物流
+#                   去评价
+#
+#                   邀请好友
+#                   去催催
+#                   确认收货
+#                   删除订单
+                }
+              ]
+            }
           end
         end
       end
