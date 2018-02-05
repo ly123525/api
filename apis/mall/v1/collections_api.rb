5fce0445ecb3bc47865e22bf42411dc6c3d5d5c4
darkwarlords <darkwarlords@sina.com>
2018-02-05 15:19:06 +0800

    no message

--- apis/mall/v1/collections_api.rb
+++ apis/mall/v1/collections_api.rb
@@ -14,7 +14,7 @@ module V1
             begin
               authenticate_user
               style = ::Mall::Style.find_uuid(params[:style_uuid])
-              ::Account::User.first.collections.find_or_create_by!(item: style)
+              @session_user.collections.find_or_create_by!(item: style)
               nil
             rescue ActiveRecord::RecordNotFound
               app_uuid_error
@@ -32,6 +32,25 @@ module V1
             begin
               authenticate_user
               present @session_user.mall_styles, with: ::V1::Entities::Mall::ProductsByStyle
+            rescue Exception => ex
+              server_error(ex)
+            end
+          end
+          
+          desc "取消收藏"
+          params do
+            requires :user_uuid, type: String, desc: '用户 UUID'
+            requires :token, type: String, desc: '用户访问令牌'
+            requires :style_uuid, type: String, desc: '商品款式 UUID'
+          end
+          delete do
+            begin
+              authenticate_user
+              style = ::Mall::Style.find_uuid(params[:style_uuid])
+              @session_user.collections.where(item: style).destroy_all
+              { result: true }
+            rescue ActiveRecord::RecordNotFound
+              app_uuid_error
             rescue Exception => ex
               server_error(ex)
             end
