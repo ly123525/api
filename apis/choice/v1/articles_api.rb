0c318d039afbfa7807d79e28c764d53f8049ca88
darkwarlords <darkwarlords@sina.com>
2018-02-10 16:27:49 +0800

    no message

--- apis/choice/v1/articles_api.rb
+++ apis/choice/v1/articles_api.rb
@@ -17,19 +17,70 @@ module V1
             end
           end
           
-          # desc "精选文章列表"
-#           params do
-#             optional :user_uuid, type: String, desc: '用户 UUID'
-#             optional :page, type: Integer, default: 1, desc: '页数'
-#           end
-#           get do
-#             begin
-#               articles = ::Choice::Article.visible.sorted
-#               present articles, with: ::V1::Entities::Choice::Articles
-#             rescue Exception => ex
-#               server_error(ex)
-#             end
-#           end
+          desc "点赞/取消"
+          params do
+            requires :user_uuid, type: String, desc: '用户 UUID'
+            requires :token, type: String, desc: '用户访问令牌'
+            requires :uuid, type: String, desc: '文章 UUID'
+            optional :cancelled, type: Boolan, default: false, desc: 'true为取消点赞'
+          end
+          put :good do
+            begin
+              authenticate_user
+              article = ::Choice::Article.find_uuid(params[:uuid])
+              if params[:cancelled]
+                article.lauds.where(user: @session_user).destroy_all rescue nil
+              else
+                article.lauds.find_or_create_by(user: @session_user).update(type: ::Choice::Lauds::Good)
+              end
+            rescue ActiveRecord::RecordNotFound
+              app_uuid_error
+            rescue Exception => ex
+              server_error(ex)
+            end
+          end
+          
+          desc "点差/取消"
+          params do
+            requires :user_uuid, type: String, desc: '用户 UUID'
+            requires :token, type: String, desc: '用户访问令牌'
+            requires :uuid, type: String, desc: '文章 UUID'
+            optional :cancelled, type: Boolan, default: false, desc: 'true为取消点差'
+          end
+          put :bad do
+            begin
+              authenticate_user
+              article = ::Choice::Article.find_uuid(params[:uuid])
+              if params[:cancelled]
+                article.lauds.where(user: @session_user).destroy_all rescue nil
+              else
+                article.lauds.find_or_create_by(user: @session_user).update(type: ::Choice::Lauds::Bad)
+              end
+            rescue ActiveRecord::RecordNotFound
+              app_uuid_error
+            rescue Exception => ex
+              server_error(ex)
+            end
+          end
+          
+          desc "分享统计"
+          params do
+            requires :user_uuid, type: String, desc: '用户 UUID'
+            requires :token, type: String, desc: '用户访问令牌'
+            requires :uuid, type: String, desc: '文章 UUID'
+          end
+          post :share_statistic do
+            begin
+              authenticate_user
+              article = ::Choice::Article.find_uuid(params[:uuid])
+              ::ShareStatistic.find_or_create_by(item: article, user: @session_user)
+              nil
+            rescue ActiveRecord::RecordNotFound
+              app_uuid_error
+            rescue Exception => ex
+              server_error(ex)
+            end
+          end
         end
       end
       
--- apis/choice/v1/entities/article.rb
+++ apis/choice/v1/entities/article.rb
@@ -29,6 +29,9 @@ module V1
         expose :scheme do |m, o|
           "lvsent://gogo.cn/web?url=" + Base64.urlsafe_encode64("http://39.107.86.17:8080/#/choice/articles?uuid=#{m.uuid}")
         end
+        expose :comments_scheme do |m, o|
+          "lvsent://gogo.cn/web?url=" + Base64.urlsafe_encode64("http://39.107.86.17:8080/#/choice/articles?uuid=#{m.uuid}")
+        end
       end
     end
   end
