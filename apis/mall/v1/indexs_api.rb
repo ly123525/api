f5982a0e0f76e709acf89e077a42969271292ae7
liuyang <182160328@qq.com>
2018-03-12 16:09:02 +0800

    no message

--- apis/mall/v1/indexs_api.rb
+++ apis/mall/v1/indexs_api.rb
@@ -16,10 +16,10 @@ module V1
               channel: {
                 background: 'https://img.alicdn.com/simba/img/TB111ELXDCWBKNjSZFtSuuC3FXa.jpg',
                 items:[
-                  {image: 'img.alicdn.com/tfs/TB1qpwlQXXXXXcCXXXXXXXXXXXX-256-256.png_60x60.jpg_.webp', scheme: 'www.baidu.com'},
-                  {image: 'img.alicdn.com/tps/i3/TB1DGkJJFXXXXaZXFXX07tlTXXX-200-200.png_60x60.jpg_.webp', scheme: 'www.baidu.com'},
-                  {image: 'img.alicdn.com/tps/i4/TB1zkDeIFXXXXXrXVXX07tlTXXX-200-200.png_60x60.jpg_.webp', scheme: 'www.baidu.com'},
-                  {image: 'img.alicdn.com/tps/i2/TB1kUwwIXXXXXXqXpXXUAkPJpXX-87-87.png_60x60.jpg_.webp', scheme: 'www.baidu.com'}
+                  {image: 'img.alicdn.com/tfs/TB1qpwlQXXXXXcCXXXXXXXXXXXX-256-256.png_60x60.jpg', scheme: 'www.baidu.com'},
+                  {image: 'img.alicdn.com/tps/i3/TB1DGkJJFXXXXaZXFXX07tlTXXX-200-200.png_60x60.jpg', scheme: 'www.baidu.com'},
+                  {image: 'img.alicdn.com/tps/i4/TB1zkDeIFXXXXXrXVXX07tlTXXX-200-200.png_60x60.jpg', scheme: 'www.baidu.com'},
+                  {image: 'img.alicdn.com/tps/i2/TB1kUwwIXXXXXXqXpXXUAkPJpXX-87-87.png_60x60.jpg', scheme: 'www.baidu.com'}
                 ]
               },
               sections: [
@@ -36,7 +36,7 @@ module V1
                   ]
                 },
                 {
-                  title_bar: {image: 'https://img.alicdn.com/simba/img/TB111ELXDCWBKNjSZFtSuuC3FXa.jpg', scheme: 'www.baidu.com'},
+                  title_bar: 'https://img.alicdn.com/simba/img/TB111ELXDCWBKNjSZFtSuuC3FXa.jpg',
                   style: 'three_column',
                   items: [ 
                     {image: 'https://img.alicdn.com/simba/img/TB1A4yuXTXYBeNkHFrdSuwiuVXa.jpg', scheme: 'www.baidu.com'},
@@ -45,7 +45,7 @@ module V1
                   ]
                 },
                 {
-                  title_bar: {image: 'https://img.alicdn.com/simba/img/TB111ELXDCWBKNjSZFtSuuC3FXa.jpg', scheme: 'www.baidu.com'},
+                  title_bar: 'https://img.alicdn.com/simba/img/TB111ELXDCWBKNjSZFtSuuC3FXa.jpg',
                   style: 'three_column',
                   items: [ 
                     {image: 'https://img.alicdn.com/simba/img/TB1A4yuXTXYBeNkHFrdSuwiuVXa.jpg', scheme: 'www.baidu.com'},
@@ -59,7 +59,16 @@ module V1
                     {image: 'https://img.alicdn.com/simba/img/TB1A4yuXTXYBeNkHFrdSuwiuVXa.jpg', scheme: 'www.baidu.com'}
                   ]
                 }
-              ]
+              ],
+              recommend: {
+                title_bar:  'https://img.alicdn.com/simba/img/TB111ELXDCWBKNjSZFtSuuC3FXa.jpg',
+                items: [
+                  {image: 'https://img.alicdn.com/simba/img/TB1A4yuXTXYBeNkHFrdSuwiuVXa.jpg', scheme: 'www.baidu.com', title: '唯新115g儿童营养猪肉松/肉酥 罐装健康辅食佐餐', price: '￥35.9'},
+                  {image: 'https://img.alicdn.com/simba/img/TB1A4yuXTXYBeNkHFrdSuwiuVXa.jpg', scheme: 'www.baidu.com', title: 'Walch/威露士衣物家居消毒液1L+健康抑菌洗手液525ml', price: '￥34.9'},
+                  {image: 'https://img.alicdn.com/simba/img/TB1A4yuXTXYBeNkHFrdSuwiuVXa.jpg', scheme: 'www.baidu.com', title: '保税日本 狮王lion CLINICA酵素珍珠美白牙膏百花+鲜果薄荷130g*2', price: '￥55.00'},
+                  {image: 'https://img.alicdn.com/simba/img/TB1A4yuXTXYBeNkHFrdSuwiuVXa.jpg', scheme: 'www.baidu.com', title: 'KENZO/高田贤三 童装 男童T恤 Q02747984 BLUE', price: '￥1078'}
+                ]
+              }
             }          
           end  
         end  
