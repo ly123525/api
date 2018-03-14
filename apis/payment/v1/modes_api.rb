1fe43d9488d79d8cd7f9b95327c91656cf5eb0c8
liuyang <182160328@qq.com>
2018-03-14 22:57:44 +0800

    no message

--- apis/payment/v1/modes_api.rb
+++ apis/payment/v1/modes_api.rb
@@ -66,12 +66,13 @@ module V1
             result = Hash.from_xml(request.body.read)["xml"]
             if WxPay::Sign.verify?(result)
               payment=::Payment.find_by(trade_no: result['out_trade_no'])
-              payment.update(paid: true, payment_at: Time.now, out_trade_no: result['transaction_id'] )
-              # payment.item.pay
-              {return_code: "SUCCESS", return_msg: 'OK'}.to_xml(root: 'xml', dasherize: false)
+              payment.update(paid: true, payment_at: Tresult['time_end'].to_time, out_trade_no: result['transaction_id'] )
+              payment.item.pay!
+              res={return_code: "SUCCESS", return_msg: 'OK'}.to_xml(root: 'xml', dasherize: false)
             else
-              {return_code: "FAIL", return_msg: "签名失败"}.to_xml(root: 'xml', dasherize: false)
+              res={return_code: "FAIL", return_msg: "签名失败"}.to_xml(root: 'xml', dasherize: false)
             end
+            res
           end      
         end
       end
