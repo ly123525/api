2e84a85bcb77b5f2b8da238b1d79b749355aabcb
liuyang <182160328@qq.com>
2018-03-15 15:54:12 +0800

    no message

--- apis/api_base.rb
+++ apis/api_base.rb
@@ -16,7 +16,7 @@ module API
         Grape::API.logger.info("==================================")
         Grape::API.logger.info(ENV["WX_OPEN_PAY_NOTIFY_URL"])
         Grape::API.logger.info("==================================")
-        return object if env["REQUEST_URI"]==(ENV["WX_OPEN_PAY_NOTIFY_URL"])
+        return object if ('http://39.107.86.17'+ env["REQUEST_URI"])==(ENV["WX_OPEN_PAY_NOTIFY_URL"])
         object.to_xml
       end
     end
