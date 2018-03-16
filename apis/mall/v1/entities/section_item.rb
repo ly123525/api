146f52e68971ec3b15829f2f1f18285bd8c5c179
liuyang <182160328@qq.com>
2018-03-16 14:13:43 +0800

    no message

--- apis/mall/v1/entities/section_item.rb
+++ apis/mall/v1/entities/section_item.rb
@@ -3,7 +3,7 @@ module V1
     module Mall
       class SectionItems < Grape::Entity
         expose :image do |m, o|
-          m.picture.image.style_url('400w') rescue nil
+          m.picture.image.url rescue nil
         end  
         expose :scheme do |m, o|
           "lvsent://gogo.cn/mall/mall_indexs/section_items?uuid=#{m.uuid}"
