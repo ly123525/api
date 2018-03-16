92dbbc36ef0222d437ce08c3299254e5bbeeacbb
liuyang <182160328@qq.com>
2018-03-16 17:38:31 +0800

    no message

--- apis/mall/v1/entities/banner.rb
+++ apis/mall/v1/entities/banner.rb
@@ -3,7 +3,7 @@ module V1
     module Mall
       class Banners < Grape::Entity
         expose :image do |m, o|
-          m.picture.image.style_url('400w') rescue nil
+          m.picture.image.style_url('640w') rescue nil
         end  
         expose :scheme do |m, o|
           "lvsent://gogo.cn/mall/mall_index/banners?uuid=#{m.uuid}"
