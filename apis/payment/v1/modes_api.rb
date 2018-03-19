366ed757d891aa4c166e2cffd4d8ed8cc620d20a
liuyang <182160328@qq.com>
2018-03-19 16:31:34 +0800

    no message

--- apis/payment/v1/modes_api.rb
+++ apis/payment/v1/modes_api.rb
@@ -120,7 +120,7 @@ module V1
               logger.info "进入回调接口"
               # notify_params = require.request_parameters
               logger.info "================================="
-              logger.info(params)
+              logger.info(params.class)
               logger.info "================================="              
               # notify_params = params.except(*request.path_parameters.keys)
               notify_params = eval(params)
