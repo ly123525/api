d58f2f110acc48a877e590e1e26efc264448dfce
liuyang <182160328@qq.com>
2018-03-26 11:17:18 +0800

    no message

--- apis/mall/v1/entities/order.rb
+++ apis/mall/v1/entities/order.rb
@@ -123,7 +123,7 @@ module V1
           "lvsent://gogo.cn/web?url=" + Base64.urlsafe_encode64("http://39.107.86.17:8080/#/payment/modes?order_uuid=#{m.uuid}") if m.created?
         end
         expose :to_refund_scheme do |m, o|
-           "lvsent://gogo.cn/web?url=" + Base64.urlsafe_encode64("http://39.107.86.17:8080/#/after_sale?order_uuid=#{m.uuid}") if (!m.created? || !m.closed? || !m.refunded?)
+           "lvsent://gogo.cn/web?url=" + Base64.urlsafe_encode64("http://39.107.86.17:8080/#/after_sale?order_uuid=#{m.uuid}") unless (m.created? || m.closed? || m.refunded?)
         end  
         expose :can_be_hasten do |m, o|
           if m.fight_group.present?
