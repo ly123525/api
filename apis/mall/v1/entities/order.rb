b47e102e84003b01a29b172f0962b0a22961ff42
liuyang <182160328@qq.com>
2018-03-29 11:46:17 +0800

    no message

--- apis/mall/v1/entities/order.rb
+++ apis/mall/v1/entities/order.rb
@@ -132,7 +132,7 @@ module V1
           "lvsent://gogo.cn/web?url=" + Base64.urlsafe_encode64("http://39.107.86.17:8080/#/cashier?order_uuid=#{m.uuid}") if (m.created? && m.expired_at >= Time.now)
         end
         expose :to_refund_scheme do |m, o|
-           "lvsent://gogo.cn/web?url=" + Base64.urlsafe_encode64("http://39.107.86.17:8080/#/after_sale?order_uuid=#{m.uuid}") unless (m.created? || m.closed? || m.refunded? || m.fight_group.try(:waiting?))
+           "lvsent://gogo.cn/web?url=" + Base64.urlsafe_encode64("http://39.107.86.17:8080/#/after_sale?order_uuid=#{m.uuid}") unless (m.created? || m.closed? || m.refunded? || m.fight_group.try(:waiting?) || m.evaluated?)
         end  
         expose :can_be_hasten do |m, o|
           if m.fight_group.present?
