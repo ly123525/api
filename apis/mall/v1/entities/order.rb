0f318790ee3102c72c61b8ee883e42be17f2a0fd
liuyang <182160328@qq.com>
2018-04-04 20:32:20 +0800

    no message

--- apis/mall/v1/entities/order.rb
+++ apis/mall/v1/entities/order.rb
@@ -268,7 +268,7 @@ module V1
             {
               title: '我在全民拼选购了商品，赶紧来拼单吧',
               image: (o[:fight_group].style.prcture.image.style_url('300w') rescue nil),
-              url: "http://39.107.86.17:8080/#/mall/products?uuid=#{m.order_items.first.product.uuid}&fight_group_uuid=#{m.fight_group_uuid}",
+              url: "http://39.107.86.17:8080/#/mall/products?uuid=#{m.order_items.first.product.uuid}&fight_group_uuid=#{o[:fight_group].uuid}",
               description: '快来拼单吧'
             }
           else
