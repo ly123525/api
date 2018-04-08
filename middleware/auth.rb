6b3f6da748519254a56e40af5b7c742f1704759c
liuyang <182160328@qq.com>
2018-04-08 20:46:49 +0800

    no message

--- middleware/auth.rb
+++ middleware/auth.rb
@@ -30,7 +30,7 @@ module API
     end
 
     def verify?(params)
-      return false unless (Time.now-5.minute..Time.now+1.second).include?( Time.at(params['timestamp'].to_i) )
+      # return false unless (Time.now-5.minute..Time.now+1.second).include?( Time.at(params['timestamp'].to_i) )
       sign = params.delete('signature')
       generate(params) == sign
     end
