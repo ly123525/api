cdf02b9c0a934a2c0acce1464ffbb0dae4a746e6
liuyang <182160328@qq.com>
2018-04-16 19:27:53 +0800

    no message

--- middleware/auth.rb
+++ middleware/auth.rb
@@ -36,7 +36,8 @@ module API
 
     def verify?(params)
       return false unless (Time.now-12.hour..Time.now+12.hour).include?( Time.at(params['timestamp'].to_i) )
-      return false if $redis.exists?(params['signature']) && $redis.read(params['signature']) == params['nonce']      
+      return false if $redis.exists?(params['signature']) && $redis.read(params['signature']) == params['nonce']
+      params.delete('image')      
       sign = params.delete('signature')
       generate(params) == sign
     end
