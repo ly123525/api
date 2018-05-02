3a1996205d6bbdd4224c39c9fa41975a3706f52f
LIHE <darkwarlords@sina.com>
2018-05-02 16:56:37 +0800

    no message

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -5,9 +5,6 @@ set :rvm_type, :user
 set :rvm_ruby_version, '2.3.3'
 set :deploy_user, 'master'
 
-# bundle 时候排除 group:test gems
-set :bundle_without, ['development' ,'test']
-
 server '39.105.15.247', user: 'master', roles: %w{web app db}, port: 22, primary: true
 
 set :deploy_to, "/srv/www/api"
--- config/deploy/staging.rb
+++ config/deploy/staging.rb
@@ -1,14 +1,9 @@
 # -*- encoding : utf-8 -*-
-set :stage, :development
-set :branch, 'development'
+set :stage, :staging
+set :branch, 'staging'
 set :rvm_type, :user
 set :rvm_ruby_version, '2.3.3'
 set :deploy_user, 'master'
-
-# bundle 时候排除 group:test gems
-set :bundle_without, ['test']
-# 开发、静默
-set :bundle_flags, '--deployment --quiet'
 
 server '47.93.35.120', user: 'master', roles: %w{web app db}, port: 22, primary: true
 
