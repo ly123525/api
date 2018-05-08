bbc7ed7abd9ea11f050d4368281bd0a31c8bb9f3
liuyang <182160328@qq.com>
2018-05-08 18:26:02 +0800

    首页抽奖进度条的假接口

--- apis/mall/v1/indexs_api.rb
+++ apis/mall/v1/indexs_api.rb
@@ -33,7 +33,20 @@ module V1
             rescue Exception => ex
               server_error(ex)
             end
-          end    
+          end
+          desc "首页抽奖进度"
+          params do
+            optional :user_uuid, type: String, desc: '用户UUID'
+          end
+          get :progress_bar do
+            begin
+              {current_foucs_on_count: 888000, target_focus_count: 1000000, scheme: "#{ENV['H5_HOST']}/#/expedite_openaward"}
+            rescue ActiveRecord::RecordNotFound
+              app_uuid_error
+            rescue Exception => ex
+              server_error(ex)
+            end            
+          end        
         end  
       end  
     end  
