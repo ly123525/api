52dfb910ea627eb06fb011e6dc271bd31b79d890
liuyang <182160328@qq.com>
2018-05-09 21:08:46 +0800

    no message

--- lib/wxpay.rb
+++ lib/wxpay.rb
@@ -7,4 +7,23 @@ module WxPay
     return {appid: ENV['WX_OPEN_APP_ID'], mch_id: ENV['WX_OPEN_MCH_ID'], key: ENV['WX_OPEN_API_KEY']} if trade_type==::WxPay::TRADE_APP
     {appid: ENV['WX_MP_APP_ID'], mch_id: ENV['WX_MP_MCH_ID'], key: ENV['WX_MP_API_KEY']}  
   end
+  module Service
+    ORDER_QUERY_REQUIRED_FIELDS = [:out_trade_no]
+    def self.order_query(params, options = {})
+      params = {
+        appid: options.delete(:appid) || WxPay.appid,
+        mch_id: options.delete(:mch_id) || WxPay.mch_id,
+        key: options.delete(:key) || WxPay.key,
+        nonce_str: SecureRandom.uuid.tr('-', '')
+      }.merge(params)
+
+
+      r = WxPay::Result.new(Hash.from_xml(invoke_remote("/pay/orderquery", make_payload(params), options)))
+      check_required_options(params, ORDER_QUERY_REQUIRED_FIELDS)
+
+      yield r if block_given?
+
+      r
+    end
+  end
 end
