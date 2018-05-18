377570b66e8dcda07517ed294a190c9b59a7288f
liuyang <182160328@qq.com>
2018-05-18 22:52:38 +0800

    no message

--- middleware/auth.rb
+++ middleware/auth.rb
@@ -4,7 +4,9 @@ module API
     def before
       return if env['PATH_INFO'].include?('/doc/swagger_doc')
       return if env['PATH_INFO'].include?('v1/wx_token_verfity.txt')
-            Grape::API.logger.info "===================#{params.to_s}"
+      request = Grape::Request.new(@env, build_params_with: @options[:build_params_with])
+      params = request.params
+      Grape::API.logger.info "===================#{params.to_s}"
       return if ENV['SERVER_ENV']=='development' || ENV['SERVER_ENV']=='staging'
       request = Grape::Request.new(@env, build_params_with: @options[:build_params_with])
       params = request.params
