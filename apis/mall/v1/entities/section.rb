c861f5b77345548053dee165cdff99110a343956
liuyang <182160328@qq.com>
2018-05-23 15:31:47 +0800

    no message

--- apis/mall/v1/entities/section.rb
+++ apis/mall/v1/entities/section.rb
@@ -11,9 +11,15 @@ module V1
         expose :items, using: ::V1::Entities::Mall::SectionItems, unless: lambda {|m, o | m.type == "Mall::Indices::Sections::WebView"} do |m, o|
           m.section_items
         end
-        expose :url, if: lambda {|m, o | m.type == "Mall::Indices::Sections::WebView"} do |m, o|
-          "#{ENV['H5_HOST']}/#/raffle/resourcelocation"
-        end        
+        expose :activity_scheme, if: lambda {|m, o | m.type == "Mall::Indices::Sections::WebView"} do |m, o|
+          "lvsent://gogo.cn/web?url=" + Base64.urlsafe_encode64("#{ENV['H5_HOST']}/#/raffle/resourcelocation")
+        end
+        expose :width, if: lambda {|m, o | m.type == "Mall::Indices::Sections::WebView"} do |m, o|
+          3
+        end
+        expose :hight, if: lambda {|m, o | m.type == "Mall::Indices::Sections::WebView"} do |m, o|
+          1
+        end            
       end  
     end   
   end  
