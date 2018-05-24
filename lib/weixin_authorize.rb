b60195827598e10a804ae37e02f2bcd4024c1c31
liuyang <182160328@qq.com>
2018-05-24 19:42:24 +0800

    no message

--- lib/weixin_authorize.rb
+++ lib/weixin_authorize.rb
@@ -1,6 +1,9 @@
 namespace = "weixin:authorize"
+redis = Redis.new(url: ENV['REDIS'])
+redis = Redis::Namespace.new(namespace, redis: redis)
 WeixinAuthorize.configure do |config|
-  config.redis = Redis.new(url: ENV['REDIS'], namespace: namespace)
+  # config.redis = Redis.new(url: ENV['REDIS'], namespace: namespace)
+  config.redis = redis
 end
 $wx_open_auth = WeixinAuthorize::Client.new(ENV["WX_OPEN_APP_ID"], ENV["WX_OPEN_APP_SECRET"])
 $wx_mp_auth = WeixinAuthorize::Client.new(ENV["WX_MP_APP_ID"], ENV["WX_MP_APP_SECRET"] )
