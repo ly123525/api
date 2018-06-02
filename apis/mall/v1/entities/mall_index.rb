48fe68660b9ecd86c85f47206bda31dd03753066
liuyang <182160328@qq.com>
2018-06-02 12:47:00 +0800

    no message

--- apis/mall/v1/entities/mall_index.rb
+++ apis/mall/v1/entities/mall_index.rb
@@ -18,7 +18,7 @@ module V1
           "#{ENV['H5_HOST']}/#/activity/popup"
         end
         expose :pop_up_image do |m, o|
-          "#{ENV['IMAGE_DOMAIN']}/app/index_pop.jpg?x-oss-process=style/240w"
+          "#{ENV['IMAGE_DOMAIN']}/app/index_pop.png?x-oss-process=style/240w"
         end
         expose :pop_up_scheme do |m, o|
           "lvsent://gogo.cn/web?url="+Base64.urlsafe_encode64("#{ENV['H5_HOST']}/#/expedite_openaward")
