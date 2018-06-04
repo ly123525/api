9c458f5aebeb77b323ebab1fadcf8c30256571c1
liuyang <182160328@qq.com>
2018-06-04 20:28:00 +0800

    no message

--- apis/mall/v1/entities/product.rb
+++ apis/mall/v1/entities/product.rb
@@ -234,7 +234,7 @@ module V1
           m.product.mini_purchase_quantity
         end
         expose :activity_lottery_tips do |m, o|
-          "拼主获得2张抽奖券,拼客获得1张抽奖券"
+          "拼单成功后拼主获得2张抽奖券,拼客获得1张抽奖券"
         end
       end
     end
