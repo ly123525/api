1fdcb47d69e1506bda05779aa6b60b42230ca780
liuyang <182160328@qq.com>
2018-06-04 19:12:45 +0800

    修改活动

--- apis/activity/v1/entities/lottery.rb
+++ apis/activity/v1/entities/lottery.rb
@@ -8,8 +8,35 @@ module V1
         expose :name do |m, o|
           m.lottery_name
         end
-        expose :category do |m, o|
-          m.class.to_s.split("::").last
+        expose :color do |m, o|
+          m.lottery_template.color
+        end   
+        expose :status_tips do |m, o|
+          if m.not_lottery?
+            "未开奖"
+          elsif m.the_winning?
+            "中奖"
+          elsif !m.not_winning?
+            "未中奖"  
+          end  
+        end
+        expose :number do |m, o|
+          m.number.join('')
+        end 
+        expose :nper do |m, o|
+          m.lottery_template.activity.sub_name
+        end
+        expose :list_url do |m, o|
+          "#{ENV['H5_HOST']}/#/raffletickets" if o[:inner_app]
+        end     
+      end 
+      
+      class Lotteries < Grape::Entity
+        expose :name do |m, o|
+          m.lottery_name
+        end
+        expose :color do |m, o|
+          m.lottery_template.color
         end  
         expose :color_status do |m ,o|
           m.not_winning?
@@ -23,47 +50,11 @@ module V1
             "未中奖"  
           end  
         end
-        expose :number
-        expose :nper do |m, o|
-          m.activity_item.activity.name
-        end
-        expose :list_url do |m, o|
-          "#{ENV['H5_HOST']}/#/raffletickets" if o[:inner_app]
-        end
-        expose :down_load_scheme do |m, o|
-          unless o[:inner_app]
-            case o[:os]
-              when 'Android'
-                "http://a.app.qq.com/o/simple.jsp?pkgname=com.lst.go"
-              when 'IOS'
-                'https://itunes.apple.com/cn/app/id1369799402?mt=8' 
-            end     
-          end  
-        end     
-      end 
-      
-      class Lotteries < Grape::Entity
-        expose :name do |m, o|
-          m.lottery_name
-        end
-        expose :category do |m, o|
-          m.class.to_s.split("::").last
+        expose :number do |m, o|
+          m.number.join('')
         end  
-        expose :color_status do |m ,o|
-          m.not_winning?
-        end  
-        expose :status_tips do |m, o|
-          if m.not_lottery?
-            "未开奖"
-          elsif m.the_winning?
-            "中奖"
-          elsif m.not_winning?
-            "未中奖"  
-          end  
-        end
-        expose :number
         expose :nper do |m, o|
-          m.activity_item.activity.name
+          m.lottery_template.activity.sub_name
         end                     
       end   
       
