2c68b780de0ac27bf8913cd861c2541a66593d72
liuyang <182160328@qq.com>
2018-07-20 23:35:25 +0800

    no message

--- apis/payment/v1/modes_api.rb
+++ apis/payment/v1/modes_api.rb
@@ -64,10 +64,10 @@ module V1
               r[:package_value] = package
               inner_app = inner_app? request
               r[:result_scheme] = "lvsent://gogo.cn/web?url=" + Base64.urlsafe_encode64("#{ENV['H5_HOST']}/#/vip/buy_result?uuid=#{order.uuid}") if params[:order_type] == ::Payment::ORDER_TYPE_VIP
-              r[:result_scheme] = "lvsent://gogo.cn/web?url=" + Base64.urlsafe_encode64("#{ENV['H5_HOST']}/#/fightgroup?fight_group_uuid=#{fight_group.uuid}") if fight_group.present? && inner_app
-              r[:result_scheme] = "lvsent://gogo.cn/web?url=" + Base64.urlsafe_encode64("#{ENV['H5_HOST']}/#/maverick/buying/success?uuid=#{order.uuid}") if !fight_group.present? && inner_app
-              r[:result_scheme] = "#{ENV['H5_HOST']}/#/fightgroup?fight_group_uuid=#{fight_group.uuid}" if fight_group.present? && !inner_app
-              r[:result_scheme] = "#{ENV['H5_HOST']}/#/maverick/buying/success?uuid=#{order.uuid}" if !fight_group.present? && !inner_app
+              r[:result_scheme] = "lvsent://gogo.cn/web?url=" + Base64.urlsafe_encode64("#{ENV['H5_HOST']}/#/fightgroup?fight_group_uuid=#{fight_group.uuid}") if fight_group.present? && inner_app && params[:order_type] == ::Payment::ORDER_TYPE_PRODUCT
+              r[:result_scheme] = "lvsent://gogo.cn/web?url=" + Base64.urlsafe_encode64("#{ENV['H5_HOST']}/#/maverick/buying/success?uuid=#{order.uuid}") if !fight_group.present? && inner_app && params[:order_type] == ::Payment::ORDER_TYPE_PRODUCT
+              r[:result_scheme] = "#{ENV['H5_HOST']}/#/fightgroup?fight_group_uuid=#{fight_group.uuid}" if fight_group.present? && !inner_app && params[:order_type] == ::Payment::ORDER_TYPE_PRODUCT
+              r[:result_scheme] = "#{ENV['H5_HOST']}/#/maverick/buying/success?uuid=#{order.uuid}" if !fight_group.present? && !inner_app && params[:order_type] == ::Payment::ORDER_TYPE_PRODUCT
               r
             rescue ActiveRecord::RecordNotFound
               app_uuid_error
@@ -124,7 +124,7 @@ module V1
               timestamp: Time.now.localtime.strftime("%Y-%m-%d %H:%M:%S"),
               notify_url: Alipay::NOTIFY_URL,
               timeout_express: "2m")
-              result_scheme = "lvsent://gogo.cn/web?url=" + Base64.urlsafe_encode64("#{ENV['H5_HOST']}/#/fightgroup?fight_group_uuid=#{fight_group.uuid}") if fight_group.present?
+              result_scheme = "lvsent://gogo.cn/web?url=" + Base64.urlsafe_encode64("#{ENV['H5_HOST']}/#/fightgroup?fight_group_uuid=#{fight_group.uuid}") if fight_group.present? && params[:order_type] == ::Payment::ORDER_TYPE_PRODUCT
               result_scheme = "lvsent://gogo.cn/web?url=" + Base64.urlsafe_encode64("#{ENV['H5_HOST']}/#/maverick/buying/success?uuid=#{order.uuid}") if  !fight_group.present? && params[:order_type] == ::Payment::ORDER_TYPE_PRODUCT
               result_scheme = "lvsent://gogo.cn/web?url=" + Base64.urlsafe_encode64("#{ENV['H5_HOST']}/#/vip/buy_result?uuid=#{order.uuid}") if params[:order_type] == ::Payment::ORDER_TYPE_VIP
               {res: res, result_scheme: result_scheme }
