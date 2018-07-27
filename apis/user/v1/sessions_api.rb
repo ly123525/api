3019bb50862281a51e5efa5483620e989cc86174
liuyang <182160328@qq.com>
2018-07-27 19:19:02 +0800

    no message

--- apis/user/v1/sessions_api.rb
+++ apis/user/v1/sessions_api.rb
@@ -62,8 +62,8 @@ module V1
               client_info_record(request, user_and_token[1])
               inviter_user = ::Account::User.find_uuid params[:inviter_uuid] rescue nil
               logger.info "====================inviter_user========#{inviter_user.present?}"
-              logger.info "====================user========#{(user_and_token[1]).id}"
-              logger.info "====================count========#{(user_and_token[1]).tokens.count}"
+              logger.info "====================user========#{(user_and_token[0]).id}"
+              logger.info "====================count========#{(user_and_token[0]).tokens.count}"
               ::Operate::CommuneHandler.work_score user_and_token[0], inviter_user
               present user_and_token[0], with: ::V1::Entities::User::UserForLogin, token: user_and_token[1].token
             rescue Exception => ex
